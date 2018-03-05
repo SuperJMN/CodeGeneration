be7aba2428f7acfeb4f6eeb16851dfadd21f50e9
José Manuel Nieto Sánchez (SuperJMN) <superjmn@outlook.com>
2018-03-05 12:19:20 +0100

    Tests

---
+++ CodeGen.Ast.Tests/TokenizerSpecs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Superpower;
+using Xunit;
+
+namespace CodeGen.Ast.Tests
+{
+    public class TokenizerSpecs
+    {
+        [Theory]
+        [MemberData(nameof(TokenData))]
+        public void TokenizationTest(string code, IEnumerable<LangToken> tokens)
+        {
+            var sut = CreateSut();
+            var actual = sut.Tokenize(code).Select(t => t.Kind);
+            var expected = tokens;
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        public static IEnumerable<object[]> TokenData()
+        {
+            return new List<object[]>()
+            {
+                new object[]{ "==", new List<LangToken>() { LangToken.DoubleEqual },  },
+                new object[]{ "=", new List<LangToken>() { LangToken.Equal },  },
+            };
+        }
+
+        private Tokenizer<LangToken> CreateSut()
+        {
+            return TokenizerFactory.Create();
+        }
+    }
+}
---
+++ CodeGen.Ast/TokenizerFactory.cs
@@ -0,0 +1,17 @@
+﻿using Superpower;
+using Superpower.Parsers;
+using Superpower.Tokenizers;
+
+namespace CodeGen.Ast
+{
+    public static class TokenizerFactory
+    {
+        public static Tokenizer<LangToken> Create()
+        {
+            return new TokenizerBuilder<LangToken>()
+                .Match(Character.EqualTo('=').IgnoreThen(Character.EqualTo('=')), LangToken.DoubleEqual)
+                .Match(Character.EqualTo('='), LangToken.Equal)
+                .Build();
+        }
+    }
+}
